891c9f56c57af3af2d0e1dda00bc0fd0380dad2e
Nguyen Ba The <nguyenbathe98@gmail.com>
2018-08-06 13:12:24 +0700

    Gem admin

--- app/models/trainee.rb
+++ app/models/trainee.rb
@@ -2,7 +2,7 @@ class Trainee < ApplicationRecord
   has_many :course_trainees
   has_many :trainee_tasks
   has_many :trainee_subjects
-  has_many :courses,  through: :course_trainee
-  has_many :tasks,    through: :trainee_task
-  has_many :subjects, through: :trainee_subject
+  has_many :courses,  through: :course_trainees
+  has_many :tasks,    through: :trainee_tasks
+  has_many :subjects, through: :trainee_subjects
 end
