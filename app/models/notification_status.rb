74f9fbe980707c46d25bd682152aaf6bc22be58a
NDK1410 <khanh.nd1410@gmail.com>
2019-03-18 21:29:44 +0700

    transaction-updating

--- app/models/notification_status.rb
+++ app/models/notification_status.rb
@@ -3,13 +3,14 @@ class NotificationStatus < ApplicationRecord
   belongs_to :notification
   belongs_to :course_trainee
   enum status: {unread: 0, readed: 1}
+
   private
   def notification_broadcast_job
-  if Trainee.current
+    if Trainee.current
       current_course_trainee = CourseTrainee.find_by trainee_id: Trainee.current.id , course_id: self.notification.course.id
       NotificationBroadcastJob.perform_later(current_course_trainee.notification_statuses.unread.count,self.notification)
+    else
+      NotificationBroadcastJob.perform_later(Notification.count,self.notification)
     end
-  else
-    NotificationBroadcastJob.perform_later(Notification.count,self.notification)
   end
 end
